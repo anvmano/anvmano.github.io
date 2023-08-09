8952d1974d2d3642676e8c7c913be00c370fba49
André Navarro <32142893+anvmano@users.noreply.github.com>
2023-08-09 15:05:10 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 // Configura o Firebase
 const firebaseConfig = {
-    apiKey: ${{secrets.CHAVE_API_FIREBASE}},
+    apiKey: ${secrets.CHAVE_API_FIREBASE},
     authDomain: "estacaometereologicaesp32.firebaseapp.com",
     databaseURL: "https://estacaometereologicaesp32-default-rtdb.firebaseio.com",
     projectId: "estacaometereologicaesp32",
